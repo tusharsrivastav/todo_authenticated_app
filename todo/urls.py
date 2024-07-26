636f7bdebac722fcea258d0f9aa1c4c926352fab
Tushar Srivastava <tusharsrivastava@Tushars-MacBook-Air.local>
2024-07-26 13:36:24 +0530

    rename directory

--- todo/urls.py
+++ todo/urls.py
@@ -1,22 +1,16 @@
-"""todo_app URL Configuration
+from django.urls import path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from . import views
 
+app_name = "todo"
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("todo.urls")),
+    path("", views.index, name="home"),
+    path("profile/", views.profile_view, name="profile"),
+    path("signup/", views.signup_view, name="signup"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("add/", views.add, name="add"),
+    path("check/<int:todo_id>", views.check, name="check"),
+    path("uncheck/<int:todo_id>", views.uncheck, name="uncheck"),
+    path("delete/<int:todo_id>", views.delete, name="delete"),
 ]
